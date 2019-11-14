e189c8f938fd7bf4980feae0333d0bf381faa06f
ravindra1234567 <47721333+ravindra1234567@users.noreply.github.com>
2019-11-14 23:53:30 +0530

    map progrm

--- map.cpp
+++ map.cpp
@@ -1,1 +1,17 @@
 #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<int , int> map1;
+
+    map1.insert(make_pair(1,10));
+    map1.insert(make_pair(2,11));
+    map<int,int>::iterator  itr;
+    for(itr= map1.begin(); itr!=map1.end(); ++itr)
+
+    {
+        cout<<itr->first<<"  "<<itr->second<<endl;
+    }
+    return 0;
+
+}
