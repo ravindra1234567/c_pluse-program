cf6a4a24b219491e506df685d35eb44e013b9587
ravindra1234567 <47721333+ravindra1234567@users.noreply.github.com>
2019-11-15 00:06:57 +0530

    dynamic map insersion

--- map.cpp
+++ map.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 int main()
 {
-    map<int , int> map1;
+    int n,key,value;
+     map<int , int> map1;
+    cout<<"Enter the how many element want to insert in map = ";
+    cin>>n;
+   for(int i=1;i<=n;i++) {
+        cout<<"Enter the Key = ";
+        cin>>key;
+        cout<<"Enter the Value = ";
+        cin>>value;
+        map1.insert(make_pair(key,value));
+    }
 
-    map1.insert(make_pair(1,10));
-    map1.insert(make_pair(2,11));
+
+
     map<int,int>::iterator  itr;
     for(itr= map1.begin(); itr!=map1.end(); ++itr)
 
     {
-        cout<<itr->first<<"  "<<itr->second<<endl;
+        cout<<"Key = "<<itr->first<<"  "<<"Value = "<<itr->second<<endl;
     }
     return 0;
 
