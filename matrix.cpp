0b48ad5fa9ee10bbc801e51ca941c733a920c7c9
ravindra1234567 <ravindrakushwahanwg@gmail.com>
2020-01-31 01:00:53 +0530

    add matrix program

---
+++ matrix.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int A[2][2], B[2][2], C[2][2];
+  cout << "Matrix A" << endl;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      cin >> A[i][j];
+    }
+  }
+  cout << "Matrix B";
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      cin >> B[i][j];
+    }
+  }
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      C[i][j] = A[i][j] + B[i][j];
+    }
+  }
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      cout << C[i][j] << "  ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
